addf1c7bc0b00424220ffdec0c36c57e5177edcc
Ruthvik M T <156601973+ruthvik-mt@users.noreply.github.com>
2025-10-18 11:37:28 +0530

    Update ProtectedRoute.tsx

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,15 +1,3 @@
-// // src/components/ProtectedRoute.tsx
-// import { Navigate } from "react-router-dom";
-// import { useAuth } from "@/hooks/useAuth";
-// import { ReactNode } from "react";
-
-// const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-//   const { token } = useAuth();
-//   return token ? <>{children}</> : <Navigate to="/login" replace />;
-// };
-
-// export default ProtectedRoute;
-
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { JSX } from "react";
