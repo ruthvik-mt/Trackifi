f61b8b1b40a3ddc0c6cd56a3b30e4139a46c4be1
Ruthvik M T <156601973+ruthvik-mt@users.noreply.github.com>
2025-10-18 11:39:11 +0530

    Update useAuth.ts

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,14 +1,3 @@
-// import { useContext } from "react";
-// import { AuthContext, AuthContextType } from "../context/AuthContext";
-
-// export const useAuth = (): AuthContextType => {
-//   const ctx = useContext(AuthContext);
-//   if (!ctx) {
-//     throw new Error("useAuth must be used within an <AuthProvider>");
-//   }
-//   return ctx;
-// };
-
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
